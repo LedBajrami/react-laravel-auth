260edb9959e5db2eaeeb99301dbcac3969df4953
Led <procomputers@PROs-MacBook.local>
2024-12-09 23:44:24 +0100

    [Backend/Auth] Add user login API with validation

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -17,4 +17,19 @@ class Useontroller extends Controller
 
         return response()->json(['message' => 'User registred succesfully', 'user' => $user], 201);
     }
+
+    public function login(LoginRequest $request) {
+        $credentials = $request->only('email', 'password');
+
+        $checkUser = Auth::attempt($credentials);
+
+        if ($checkUser) {
+            $user = Auth::user();
+            $token = $user->createToken('access_token')->accessToken;
+
+            return response()->json(['message' => 'User logged in succesfully', 'user' => $user, 'token' => $token], 201);
+        } else {
+            return response()->json(['message' => 'User is not authorized'], 401);
+        }
+    }
 }
