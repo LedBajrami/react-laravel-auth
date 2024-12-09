ccd574b85805246ecc4d4b521b5f39e943730b7d
Led <procomputers@PROs-MacBook.local>
2024-12-09 23:59:40 +0100

    [Backend/Auth] Add routes for registration, login, user retrieval, photo upload and logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+
+Route::group([], function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+});
+
+
+Route::middleware(['auth:api'])->group(function() {
+
+    Route::get('/user', [UserController::class, 'user']);
+    Route::post('/user/upload', [UserController::class, 'uploadPhoto']);
+    Route::get('/logout', [UserController::class, 'logout']);
+    
+});    
